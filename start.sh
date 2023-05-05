f069dcdd8dbfbee2964d0e8f5549ae60103c19e1
Radosław Woźniak <radoslaw.wozniak2000@gmail.com>
2023-05-05 20:21:36 +0200

    Allow start.sh to be sourceable again

--- start.sh
+++ start.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 python3 -m venv pyenv
 source pyenv/bin/activate
-pip install --require-virtualenv -r requirements.txt > /dev/null
-if [ -n "$1" ]; then
-  ./setup_graylog.py "$@"
-elif [ -n "$ELEMENTS" ]; then
-  ./setup_graylog.py $ELEMENTS
-else
-  printf "Didn't supply root element names\n"
+pip install --require-virtualenv -r requirements.txt &> /dev/null
+if [ "$?" != "0" ]; then
+  printf "Installing requirements failed"
   exit 2
+fi
+if [ "$0" = "$BASH_SOURCE" ]; then
+  if [ -n "$1" ]; then
+    ./setup_graylog.py "$@"
+  elif [ -n "$ELEMENTS" ]; then
+    ./setup_graylog.py $ELEMENTS
+  else
+    printf "Didn't supply root element names\n"
+    exit 2
+  fi
 fi
